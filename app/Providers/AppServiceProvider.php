52264ffb10d31cb6e65fd7149570c612197369e7
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-12-01 20:20:42 -0500

    fix: horarrio del service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        date_default_timezone_set('America/Lima');
+        date_default_timezone_set('America/Caracas');
 
         // if (request()->server->has('HTTP_X_ORIGINAL_HOST')) {
         //     url()->forceRootUrl(
