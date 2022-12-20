d70561ac2f90dc05b43d45d9604750377ea1fa28
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-12-20 10:34:24 -0500

    fix: chart detallado 2

--- app/Http/Controllers/JugadasController.php
+++ app/Http/Controllers/JugadasController.php
@@ -81,7 +81,7 @@ class JugadasController extends Controller
         LEFT JOIN sorteos_types ON register_details.sorteo_type_id = sorteos_types.id
         WHERE DATE(register_details.created_at) = DATE(?)
         and sorteo_type_id = ?
-        and schedule_id = ?
+        and schedule = ?
         and register_details.moneda_id = 1
         group by createdAt", [$created_at, $created_at, $created_at, $loteria_id, $schedule]);
 
