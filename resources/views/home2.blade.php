554084eccae5e8392e5f494b66e35bf5cee144b8
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2023-05-29 17:15:49 -0500

    fix: mensaje de lotto activo regalos

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -22,8 +22,9 @@
                     </div>
                     @endif
 
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Lotto Activo</strong> se encuentra desactivado temporalmente.
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Lotto Activo Regalos</strong> ya se encuentra disponible 🎉🎈 <br>
+                         Ponte en contacto con tu administrador en caso de no tener disponible esta loteria.
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
 
