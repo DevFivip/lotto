0f7086e1c8b5629e26ffb801449c1e33b61475e0
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2024-12-04 10:16:15 -0500

    feat:

--- app/Console/Commands/SetSorteoCommand.php
+++ app/Console/Commands/SetSorteoCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Config;
 use App\Http\Libs\Telegram;
 use App\Models\Schedule;
 use Illuminate\Console\Command;
@@ -31,25 +32,22 @@ class SetSorteoCommand extends Command
     public function handle()
     {
         $telegram = new Telegram();
-        $s = Schedule::where('status', 1)->where('sorteo_type_id', '=', 1)->first(); // Cerrar Lotto Activo  
-        if (!!$s) {
-            $s->status = 0;
-            $s->update();
-            $telegram->sendMessage('✔ Horario Cerrado ' . $s->schedule . '  Lotto Activo');
-            return $s->schedule . ' ' . 'off';
+        $s2 = Schedule::where('status', 1)->whereNotIn('id',Config::BANEDSCHEDULES)->where('sorteo_type_id', '=', 1)->first(); // Cerrar Lotto Activo  
+        if (!!$s2) {
+            $s2->status = 0;
+            $s2->update();
+            $telegram->sendMessage('✔ Horario Cerrado ' . $s2->schedule . ' Lotto Activo');
+            return $s2->schedule . ' ' . 'off';
         } else {
-
-            $sorteos = Schedule::where('sorteo_type_id', '=', 1)->get();
-            foreach ($sorteos as $sorteo) {
-                $sorteo->status = 1;
-                $sorteo->update();
+            $sorteos2 = Schedule::whereNotIn('id',Config::BANEDSCHEDULES)->where('sorteo_type_id', '=', 1)->get();
+            foreach ($sorteos2 as $sorteo2) {
+                $sorteo2->status = 1;
+                $sorteo2->update();
             }
             $telegram->sendMessage('✔ Horarios Reiniciados Lotto Activo');
             return 'reset off';
         }
 
-
-
-        return 'reset off';
+        return 0;
     }
 }
